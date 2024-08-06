af8485bcc1b263f30f8fcc74d7aa3f16d17404d8
Dipak-Prasad <dipakprasad.0078@gmail.com>
2024-08-06 16:59:17 +0530

    pic-prism

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+
+const Home = () => {
+  return (
+    <div className="mt-36">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
